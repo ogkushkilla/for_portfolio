a5266df68340bb13129d0517761b7f703dfdf163
ogkushkilla <vsevolod.kravchenko@yandex.ru>
2020-09-08 23:15:05 +0500

    сделал слайдеры

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.special-pagination',
+        bulletClass: 'special-bullet',
+        bulletActiveClass: 'special-bullet-active',
+        clickable: true
+      },
+});
